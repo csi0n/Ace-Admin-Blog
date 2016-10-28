65bb3e8ea97d84bec2c8638919700df0558364a8
csi0n <huaqing.chen@bioon.com>
2016-10-28 14:05:14 +0800

    .

--- database/migrations/2016_10_28_072441_create_comments_table.php
+++ database/migrations/2016_10_28_072441_create_comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommontsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
